e950a8423af8321b0bc10045cb2e0d5bc8e888f3
edwardsCNC <nicholas.lee.edwards@gmail.com>
2018-08-01 22:21:09 -0400

    Get data and display markers in the UI
    This commit sets up an AJAX call to the server. When the server has responded, a new Google Maps Map instance is instantiated and placed in the UI. Each datapoint in the response body is translated into a Marker on the map with the species common name as the marker title.

--- js/map.js
+++ js/map.js
@@ -1,6 +1,32 @@
+// The production URL is not included here to keep it out of version control and
+// out of public view. -->
+var url = 'URL';
+var mapCenter = { lat: 43.712629, lng: -72.261310 };
+
 function initMap() {
-  const map = new google.maps.Map(document.getElementById('map'), {
-  center: { lat: 43.712629, lng: -72.261310 },
-  zoom: 15
-});
+  $.ajax({url: url, success: function(response) {
+    // When the AJAX call to the server has responded...
+    // Put a map in the UI
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: mapCenter,
+      zoom: 15
+    });
+
+    // Iterate through the data in the response body
+    // For each datapoint, display a marker on the map
+    // When the mouse hovers over the marker, the species common name should
+    //   be displayed
+    response.forEach(function(observation) {
+      var latLng = {
+        lat: parseFloat(observation.latitude),
+        lng: parseFloat(observation.longitude)
+      };
+      console.log(observation.species_common_name)
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: observation.species_common_name
+      });
+    });
+  }});
 }
