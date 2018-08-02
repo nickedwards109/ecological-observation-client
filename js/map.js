de0fd61a59885a25ec7739c6a9ab7baa4d280fb8
edwardsCNC <nicholas.lee.edwards@gmail.com>
2018-08-01 22:47:46 -0400

    Display an informational window when a marker is clicked
    When a user clicks on a marker representing an observation, an informational window appears next to the marker. The displayed information is the common name and scientific name of the observation's species, and the latitude and longitude.

--- js/map.js
+++ js/map.js
@@ -1,31 +1,45 @@
-// The production URL is not included here to keep it out of version control and
-// out of public view. -->
+// The production URL and map center are not included here to keep them
+// out of version control and out of public view. -->
 var url = 'URL';
-var mapCenter = { lat: 43.712629, lng: -72.261310 };
+var mapCenter = { lat: 'lat', lng: 'lng'};
 
 function initMap() {
   $.ajax({url: url, success: function(response) {
     // When the AJAX call to the server has responded...
-    // Put a map in the UI
+    // Put a map in the UI.
     const map = new google.maps.Map(document.getElementById('map'), {
       center: mapCenter,
       zoom: 15
     });
 
-    // Iterate through the data in the response body
+    // Iterate through the data in the response body.
     // For each datapoint, display a marker on the map
     // When the mouse hovers over the marker, the species common name should
     //   be displayed
     response.forEach(function(observation) {
+
       var latLng = {
         lat: parseFloat(observation.latitude),
         lng: parseFloat(observation.longitude)
       };
-      console.log(observation.species_common_name)
+
+      var contentString = '<div>Common Name: ' + observation.species_common_name + '</div>'
+        + '<div>Scientific Name: ' + observation.species_scientific_name + '</div>'
+        + '<div>Latitude: ' + observation.latitude + '</div>'
+        + '<div>Longitude: ' + observation.longitude + '</div>';
+
+      var infoWindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
       var marker = new google.maps.Marker({
         position: latLng,
         map: map,
         title: observation.species_common_name
+      });
+
+      marker.addListener('click', function() {
+        infoWindow.open(map, marker);
       });
     });
   }});
